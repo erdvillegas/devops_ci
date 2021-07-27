7bd8c02fb216e5c34d8808a1a75745046a5908db
Erik Villegas <erdvillegas@gmail.com>
2021-07-27 01:48:40 -0500

    Editing Test again

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String message = "default message cambio test devOps";
+		String message = "default message cambio test DevOps";
 		Assert.assertNotNull(message);
 	}
 
