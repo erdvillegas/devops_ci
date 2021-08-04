d726b95acec4758dabd4bd52ee5340ae2ed177cc
Erik Villegas <erdvillegas@gmail.com>
2021-08-04 01:50:00 -0500

    Adding slack test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -30,28 +30,41 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 /**
  * @author sotobotero This set of test allow check the behavior of the web layer
- * application listening like as it would do in production, sending and http
- * requests and assert if method was called and http status is the expected. We
- * are using spring MVC test framework to perfom integration tests
- * A context can be said as the running environment that is provided to the current unit of work. It may be the environment variables, instance variables, state of the classes, and so on.
- * In Spring web applications, there are two contexts that gets initialized at server startup, each of which is configured and initialized differently. One is the “Application Context” and the other is the “Web Application Context“
- *Mockito is a mocking framework, JAVA-based library that is used for effective unit testing of JAVA applications. Mockito is used to mock interfaces so that a dummy functionality can be added to a mock interface that can be used in unit testing. 
- * */
+ *         application listening like as it would do in production, sending and
+ *         http requests and assert if method was called and http status is the
+ *         expected. We are using spring MVC test framework to perfom
+ *         integration tests A context can be said as the running environment
+ *         that is provided to the current unit of work. It may be the
+ *         environment variables, instance variables, state of the classes, and
+ *         so on. In Spring web applications, there are two contexts that gets
+ *         initialized at server startup, each of which is configured and
+ *         initialized differently. One is the “Application Context” and the
+ *         other is the “Web Application Context“ Mockito is a mocking
+ *         framework, JAVA-based library that is used for effective unit testing
+ *         of JAVA applications. Mockito is used to mock interfaces so that a
+ *         dummy functionality can be added to a mock interface that can be used
+ *         in unit testing.
+ */
 @WebMvcTest(InvoiceRestController.class)
-/*allow test only http incoming request layer without start the server, 
-        spring boot instatiates only the InvoiceRestController rather than the whole context*/
-@ExtendWith(SpringExtension.class)//junit5 suport extension interface hrough which classes can integrate with the JUnit test.
-@AutoConfigureMockMvc/*allow test only http incoming request layer without start the serve, 
-        but starting the full spring application context*/
+/*
+ * allow test only http incoming request layer without start the server, spring
+ * boot instatiates only the InvoiceRestController rather than the whole context
+ */
+@ExtendWith(SpringExtension.class) // junit5 suport extension interface hrough which classes can integrate with the
+                                   // JUnit test.
+@AutoConfigureMockMvc /*
+                       * allow test only http incoming request layer without start the serve, but
+                       * starting the full spring application context
+                       */
 public class BasicApplicationTests {
-  
+
     @Autowired
     private MockMvc mockMvc;
-    @MockBean //mock the repository layer in order to have a unit test for weblayer 
+    @MockBean // mock the repository layer in order to have a unit test for weblayer
     private InvoiceRepository ir;
-    @MockBean //mock the mapper layer in order to have a unit test for weblayer 
+    @MockBean // mock the mapper layer in order to have a unit test for weblayer
     InvoiceRequestMapper irm;
-    @MockBean //mock the mapper layer in order to have a unit test for weblayer 
+    @MockBean // mock the mapper layer in order to have a unit test for weblayer
     InvoiceResposeMapper irspm;
     private static final String PASSWORD = "admin";
     private static final String USER = "admin";
@@ -67,7 +80,7 @@ public class BasicApplicationTests {
     }
 
     /**
-     * Test call of create method, on weblayer. 
+     * Test call of create method, on weblayer.
      */
     @Test
     public void testCreate() throws Exception {
@@ -77,10 +90,10 @@ public class BasicApplicationTests {
         Mockito.when(ir.save(mockdto)).thenReturn(mockdto);
         Mockito.when(irm.InvoiceRequestToInvoice(new InvoiceRequest())).thenReturn(mockdto);
         Mockito.when(irspm.InvoiceToInvoiceRespose(mockdto)).thenReturn(new InvoiceResponse());
-        this.mockMvc.perform(post("/billing").header("Authorization", "Basic " + encoding)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(mockdto))
-        ).andDo(print()).andExpect(status().isOk());
+        this.mockMvc
+                .perform(post("/billing").header("Authorization", "Basic " + encoding)
+                        .contentType(MediaType.APPLICATION_JSON).content(asJsonString(mockdto)))
+                .andDo(print()).andExpect(status().isOk());
     }
 
     /**
@@ -97,10 +110,10 @@ public class BasicApplicationTests {
         InvoiceResponse invoiceResponse = new InvoiceResponse();
         invoiceResponse.setInvoiceId(1);
         Mockito.when(irspm.InvoiceToInvoiceRespose(mockdto)).thenReturn(invoiceResponse);
-        this.mockMvc.perform(get("/billing/{id}", mockdto.getId()).header("Authorization", "Basic " + encoding)
-                .accept(MediaType.APPLICATION_JSON)               
-        ).andDo(print()).andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print())
+        this.mockMvc
+                .perform(get("/billing/{id}", mockdto.getId()).header("Authorization", "Basic " + encoding)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.invoiceId").value(1));
     }
 
